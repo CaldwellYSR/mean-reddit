c2836d53c9898b7e0e85ed7c9956395eeb89d20e
Matthew Caldwell <caldwellysr@gmail.com>
2016-10-02 13:33:26 -0400

    SASS Changes and finally fixed the component bindings

--- src/post/post.controller.js
+++ src/post/post.controller.js
@@ -1,5 +1,6 @@
 angular.module("meanreddit").controller("postCtrl", [function() {
     this.post = {
+        id: this.postid,
         title: "Hello, World",
         content: "Hey guys and gals, this is a test and I sure hope it works",
         upvotes: 0
